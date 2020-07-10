05608f00a4c6252d99ea2996ffc7436ef18f43c9
deve023 <adevedia@fi.uba.ar>
2020-07-09 21:05:07 -0300

    Estetica

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -59,13 +59,18 @@ const Chat = ({ location }) => {
     console.log(message, messages);
 
     return (
-        <div className="outerContainer">
-            <div className="container">
-                <InfoBar room={room}/>
-                <Messages messages={messages} name={name}/>
-                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+        <div className="chat">
+            <div className="title">
+                <h1 className="heading"><span role="img" aria-label="donut">🍩</span> Donut-Chat <span role="img" aria-label="donut">🍩</span></h1>
             </div>
-            <TextContainer users={users} />
+            <div className="outerContainer">
+                <div className="container">
+                    <InfoBar room={room}/>
+                    <Messages messages={messages} name={name}/>
+                    <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+                </div>
+                <TextContainer users={users} />
+            </div>
         </div>
     )
 }
